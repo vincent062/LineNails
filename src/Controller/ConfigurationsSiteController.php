47803f09222f253d41a5a7088a7c4f632b7098f8
vincent <vince062580@gmail.com>
2025-09-11 12:04:38 +0200

    bouton configuration nav bar

--- src/Controller/ConfigurationsSiteController.php
+++ src/Controller/ConfigurationsSiteController.php
@@ -10,8 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/configurations/site')]
+#[IsGranted('ROLE_ADMIN')]
 final class ConfigurationsSiteController extends AbstractController
 {
     #[Route(name: 'app_configurations_site_index', methods: ['GET'])]
