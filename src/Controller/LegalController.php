6313a876b84aa0624a648429043930a4f0172090
vincent <vince062580@gmail.com>
2025-09-11 11:28:01 +0200

    mis en place RGPD

---
+++ src/Controller/LegalController.php
@@ -0,0 +1,23 @@
+<?php
+// src/Controller/LegalController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LegalController extends AbstractController
+{
+    #[Route('/mentions-legales', name: 'app_legal_mentions')]
+    public function mentions(): Response
+    {
+        return $this->render('legal/mentions.html.twig');
+    }
+
+     #[Route('/politique-de-confidentialite', name: 'app_legal_privacy')]
+    public function privacy(): Response
+    {
+        return $this->render('legal/privacy.html.twig');
+    }
+}
