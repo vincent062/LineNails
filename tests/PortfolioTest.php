fc0f80e5e01bf9cc1c1e7aaa01db3c63836253cc
vincent <vince062580@gmail.com>
2025-10-27 09:35:32 +0100

    description test

--- tests/PortfolioTest.php
+++ tests/PortfolioTest.php
@@ -10,8 +10,8 @@ class PortfolioTest extends WebTestCase
 {
     public function testSomething(): void
     {
-        $client = static::createClient();
-        $client->request('GET', '/portfolio');
-        $this->assertResponseIsSuccessful();
+        $client = static::createClient();  // Création d'un client test
+        $client->request('GET', '/portfolio'); // Simuler une requéte avec le client
+        $this->assertResponseIsSuccessful(); // Signifie que la page Portfolio se charge sans erreur serveur
     }
 }
